94cc6d56478f435d1d8e604578419a13ab93fcfa
Pedro Lucas Resente Siqueira Campos <pedrosicampos08@gmail.com>
2022-06-27 13:21:18 -0300

    Initial commit

---
+++ parte1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <math.h>
+
+double dados[][] = 
+    {
+     /* x  F(x) O(x)  F(x)cos(O(x)) */
+        { 0  0    .5  0     };
+        { 5  9   1.4  1.5297};    
+        {10 13    .75 9.5120};
+        {15 14    .9  8.7025};
+        {20 10.5 1.3  2.8087};
+        {25 12   1.48 1.0881};
+        {30  5   1.5   .3537};
+    };
+
+void barycentricWeights(double **weight) {
+    int n = 7;
+    double *aux;
+    aux = new double[
+    
+    for (int i = 0; i < n; i++) {
+        if (i 
+    }
+
+    return;
+}
+
+int main() {
+    return 0;
+}
+
