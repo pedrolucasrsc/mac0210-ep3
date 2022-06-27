2b2d0a906eddf492a87891449b92141ffa2e5367
Pedro Lucas Resente Siqueira Campos <pedrosicampos08@gmail.com>
2022-06-27 14:59:17 -0300

    finished avalLagrange

---
+++ interpol.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+//int n = 7;
+double dados[][4] = 
+    {
+     /* x  F(x) O(x)  F(x)cos(O(x)) */
+        { 0,  0,    .5,  0     },
+        { 5,  9,   1.4,  1.5297},    
+        {10, 13,    .75, 9.5120},
+        {15, 14,    .9,  8.7025},
+        {20, 10.5, 1.3,  2.8087},
+        {25, 12,   1.48, 1.0881},
+        {30,  5,   1.5,   .3538}
+    };
+
+void barycentricWeights(double **weight) {
+    int n = 7;
+    double aux[n];
+    for (int j = 0; j < n; j++)
+        aux[j] = 1;
+    
+    for (int j = 0; j < n; j++)
+        for (int i = 0; i < n; i++) {
+            if (i == j) continue;
+
+            aux[j] *= (dados[j][0] - dados[i][0]);
+        }
+
+    for (int j = 0; j < n; j++)
+        (*weight)[j] = 1/aux[j];
+
+    return;
+}
+
+double avalLagrange(double x) {
+    int n = 7;
+
+    // x == xi? i = 0...n-1
+    for (int i = 0; i < n; i++)
+        if (x == dados[i][0])
+            return dados[i][3];
+
+    // x < x0 || xn-1 < x ?
+    if (x < dados[0][0] || dados[6][0] < x)
+        return NAN;
+
+    double *weight, phi = 1, p = 0;
+    weight = malloc(n * sizeof(double));
+
+    barycentricWeights(&weight);
+
+    for (int i = 0; i < n; i++)
+        phi *= (x - dados[i][0]);
+
+    for (int j = 0; j < n; j++)
+        p += weight[j]*dados[j][3]/(x-dados[j][0]);
+    
+    return p*phi;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2)
+        return 1;
+
+    double x;
+    sscanf(argv[1],"%lf",&x);
+    printf("%lf\n",avalLagrange(x));
+
+    
+    return 0;
+}
+
